40c62b1bf18e0553e5c8e881bafd62f36876cd8f
NellyLy <69831487+NellyLy@users.noreply.github.com>
2020-08-19 12:47:27 +0400

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+
+namespace calkulator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            while (true)
+            {
+                double x, y;
+                string c;
+                Console.WriteLine("x=");
+                x = double.Parse(Console.ReadLine());
+
+                Console.WriteLine("y=");
+                y = double.Parse(Console.ReadLine());
+
+                Console.WriteLine("+,-,*,/");
+                c = Console.ReadLine();
+
+                if (c == "+")
+                {
+                    Console.WriteLine(x + y);
+                }
+                else if (c == "-")
+                {
+                    Console.WriteLine(x - y);
+                }
+                else if (c == "*")
+                {
+                    Console.WriteLine(x * y);
+                }
+                else if (c == "/")
+                {
+                    if (y == 0)
+                    {
+                        Console.WriteLine("you can't always get what you whant");
+                    }
+                    else
+                    {
+                        Console.WriteLine(x / y);
+                    }
+
+                }
+                else
+                {
+                    Console.WriteLine("try again with '+',-','*','/'");
+                }
+
+
+                Console.ReadLine();
+            }
+        }
+    }
+}
