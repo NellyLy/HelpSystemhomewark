c562431a89c4bd5c20db1aeb05a344354395e898
NellyLy <69831487+NellyLy@users.noreply.github.com>
2020-08-19 11:56:34 +0400

    Add files via upload

---
+++ current.cs
@@ -0,0 +1,18 @@
+using System;
+
+namespace Hellowarld
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+           Console.ReadKey();
+       	   }
+		    static void Nellytest() 
+			 {
+            Console.WriteLine("Hello World! hello mememme");
+           Console.ReadKey();
+       	   }
+    }
+}
